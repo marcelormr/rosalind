c1baac5bac5ca0f9f1e0daa9bc7f66c7c3bf14e5
Marcelo <celo.rmr@gmail.com>
2026-04-08 07:18:16 -0300

    Initial commit with first Rosalind Python solutions

---
+++ Rosalind_ini2.py
@@ -0,0 +1,6 @@
+import math
+
+a = 947
+b = 915
+h = a*a + b*b
+print(h)
---
+++ Rosalind_ini3.py
@@ -0,0 +1,7 @@
+s = 'mv2qdnjPsammophis71x1PurDIiKnzpBvbetcmw56hErEJYxgCO47mlaVbtMyyLEWelFwTVP63JZBopAD1H0GCf2P52914Uqa8NSLh01WsTBGuPHoBPzR7hmxDQxLj40naWYqFxOaehYjNSqgjwrquinquetaeniataaKnRapF28PRIGYevmA.'
+a = 7
+b = 16
+c = 148
+d = 162
+slice = s[a:b+1] + ' ' + s[c:d+1]
+print(slice)
