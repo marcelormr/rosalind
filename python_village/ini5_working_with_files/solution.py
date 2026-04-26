fa96ebdb43473603d34e7208a1e9182946e6edaf
Marcelo <celo.rmr@gmail.com>
2026-04-26 18:41:25 -0300

    Refactor INI5 file handling solution for cleaner I/O and indexing

--- python_village/ini5_working_with_files/solution.py
+++ python_village/ini5_working_with_files/solution.py
@@ -1,10 +1,11 @@
-inputFile = open('rosalind_ini5_input.txt', 'r')
-inputFile = inputFile.readlines()
-outputFile = open('rosalind_ini5_output.txt', 'w')
+input_file = open('rosalind_ini5_input.txt', 'r')
+output_file = open('rosalind_ini5_output.txt', 'w')
 
-lineCounter = 0
+line_counter = 0
 
-for line in inputFile:
-    if lineCounter %2 == 1:
-        outputFile.write(str(line))
-    lineCounter += 1
+for line in input_file:
+    if line_counter % 2 == 1:
+        output_file.write(line)
+    line_counter += 1
+input_file.close()
+output_file.close()
