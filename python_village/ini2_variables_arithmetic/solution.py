ab982cf466dd01e170095c3f47eb50547eae53ad
Marcelo <celo.rmr@gmail.com>
2026-05-15 18:47:29 -0300

    Remove obsolete pre-standardization Python Village folders

--- python_village/ini2_variables_arithmetic/solution.py
+++
@@ -1,4 +0,0 @@
-a = 947
-b = 915
-hypotenuse_squared = a * a + b * b
-print(hypotenuse_squared)   
--- python_village/ini4_conditions_loops/solution.py
+++
@@ -1,7 +0,0 @@
-a = 4267
-b = 9241
-total = 0
-for i in range(a,b+1):
-    if i % 2 == 1:
-        total += i
-print(total)
